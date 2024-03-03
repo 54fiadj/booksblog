ca32b2fbb659876bb06b5916b79717abd577272d
54fiadj <54fiadj@gmail.com>
2024-03-03 20:58:42 +0100

    updated build

--- booksblog/build.sh
+++ booksblog/build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python mywebsite/manage.py collectstatic --no-input
+python mywebsite/manage.py migrate
